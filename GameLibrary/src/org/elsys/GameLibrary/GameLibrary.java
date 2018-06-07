0233cc50bdde6abf85f0e051cf12b4495bd0b5e4
toniuyt123 <never_toni@abv.bg>
2018-06-08 00:14:33 +0300

    we got set up

--- GameLibrary/src/org/elsys/GameLibrary/GameLibrary.java
+++ GameLibrary/src/org/elsys/GameLibrary/GameLibrary.java
@@ -6,77 +6,87 @@ public class GameLibrary {
 	public static void createDatabase(Connection conn) {
 		try {
 			Statement createQuery = conn.createStatement();
-			createQuery.execute("CREATE DATABASE GameLibrary");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameLibrary?user=root&password=root");
-			
-			Statement query = conn.createStatement();			
-			query.execute("CREATE TABLE Publishers ("
-					+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-					+ "Name VARCHAR(100) NOT NULL,"
-					+ "EstablishedDate DATE NOT NULL"
-					+ ");");
-			
-			query.execute("CREATE TABLE RatingAsWord ("
-					+ "RangeStart NUMERIC(4,2) NOT NULL,"
-					+ "RangeEnd NUMERIC(4,2) NOT NULL,"
-					+ "RatingAsWord VARCHAR(15) NOT NULL"
-					+ ");");
-			
-			query.execute("CREATE TABLE Tags("
-					+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-					+ "Tag VARCHAR(30) NOT NULL"
-					+ ");");
-			
-			query.execute("CREATE TABLE Statuses("
-					+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-					+ "Status VARCHAR(30) NOT NULL"
-					+ ");");
-			
-			query.execute("CREATE TABLE Games ("
-					+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-					+ "Title VARCHAR(100),"
-					+ "PublisherId INTEGER NOT NULL,"
-					+ "ReleaseDate DATE NOT NULL,"
-					+ "Rating NUMERIC(4, 2),"
-					+ "Status INTEGER,"
-					+ "FOREIGN KEY(PublisherId) REFERENCES Publishers(Id),"
-					+ "FOREIGN KEY(Status) REFERENCES Statuses(Id)"
-					+ ");");
-			query.execute("CREATE TABLE GamesTags("
-					+ "GameId INTEGER NOT NULL,"
-					+ "TagId INTEGER NOT NULL,"
-					+ "PRIMARY KEY (GameId, TagId),"
-					+ "FOREIGN KEY(GameId) REFERENCES Games(Id),"
-					+ "FOREIGN KEY(TagId) REFERENCES Tags(Id)"
-					+ ");");
-			
-			conn.setAutoCommit(false);
-			try {
-				String ratingSql = "INSERT INTO RatingAsWord VALUES(?, ?, ?)";
-				PreparedStatement ratingSt = conn.prepareStatement(ratingSql);
-				String ratings[] = {"Appaling", "Horrible", "Very Bad", "Bad", "Average", "Fine", "Good", "Very Good", "Great", "Masterpiece"};
-				System.out.println(ratings.length);
-				for(int i = 0;i < ratings.length;i++) {
-					System.out.println(i);
-					ratingSt.setObject(1, i, java.sql.Types.NUMERIC);
-					ratingSt.setObject(2, i + 1, java.sql.Types.NUMERIC);
-					ratingSt.setString(3, ratings[i]);
-					ratingSt.executeUpdate();
-				}
+			ResultSet rs = createQuery.executeQuery("SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'GameLibrary'");
+			rs.next();
+			if(rs.getInt("COUNT(*)") <= 0) {
+				createQuery.execute("CREATE DATABASE GameLibrary");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameLibrary?user=root&password=root");
 				
-				conn.commit();
-			} catch (Exception e){
-				conn.rollback();
-				throw e;
-			} finally {
+				Statement query = conn.createStatement();			
+				query.execute("CREATE TABLE Publishers ("
+						+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+						+ "Name VARCHAR(100) NOT NULL,"
+						+ "EstablishedDate DATE NOT NULL"
+						+ ");");
+				
+				query.execute("CREATE TABLE RatingAsWord ("
+						+ "RangeStart NUMERIC(4,2) NOT NULL,"
+						+ "RangeEnd NUMERIC(4,2) NOT NULL,"
+						+ "RatingAsWord VARCHAR(15) NOT NULL"
+						+ ");");
+				
+				query.execute("CREATE TABLE Tags("
+						+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+						+ "Tag VARCHAR(30) NOT NULL"
+						+ ");");
+				
+				query.execute("CREATE TABLE Statuses("
+						+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+						+ "Status VARCHAR(30) NOT NULL"
+						+ ");");
+				
+				query.execute("CREATE TABLE Games ("
+						+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+						+ "Title VARCHAR(100),"
+						+ "PublisherId INTEGER NOT NULL,"
+						+ "ReleaseDate DATE NOT NULL,"
+						+ "Rating NUMERIC(4, 2),"
+						+ "Status INTEGER,"
+						+ "FOREIGN KEY(PublisherId) REFERENCES Publishers(Id),"
+						+ "FOREIGN KEY(Status) REFERENCES Statuses(Id)"
+						+ ");");
+				
+				query.execute("CREATE TABLE GamesTags("
+						+ "GameId INTEGER NOT NULL,"
+						+ "TagId INTEGER NOT NULL,"
+						+ "PRIMARY KEY (GameId, TagId),"
+						+ "FOREIGN KEY(GameId) REFERENCES Games(Id),"
+						+ "FOREIGN KEY(TagId) REFERENCES Tags(Id)"
+						+ ");");
+				
 				conn.setAutoCommit(false);
+				try {
+					String insertSql = "INSERT INTO RatingAsWord VALUES(?, ?, ?)";
+					PreparedStatement prp = conn.prepareStatement(insertSql);
+					String ratings[] = {"Appaling", "Horrible", "Very Bad", "Bad", "Average", "Fine", "Good", "Very Good", "Great", "Masterpiece"};
+					for(int i = 0;i < ratings.length;i++) {
+						prp.setObject(1, i, java.sql.Types.NUMERIC);
+						prp.setObject(2, i + 1, java.sql.Types.NUMERIC);
+						prp.setString(3, ratings[i]);
+						prp.executeUpdate();
+					}
+					
+					insertSql = "INSERT INTO Statuses(Status) VALUES(?)";
+					prp = conn.prepareStatement(insertSql);
+					String statuses[] = {"Plan to Play", "Playing", "Completed"};
+					for(int i = 0;i < statuses.length;i++) {
+						prp.setString(1, statuses[i]);
+						prp.executeUpdate();
+					}
+					conn.commit();
+				} catch (Exception e){
+					conn.rollback();
+					throw e;
+				} finally {
+					conn.setAutoCommit(false);
+				}
 			}
-			/*PreparedStatement prQuery = conn.prepareStatement("INSERT INTO Games(Title) VALUES (?)");
-			prQuery.setString(1, "Fortnite");
-			int affectedRows = prQuery.executeUpdate();
-			System.out.println(affectedRows);*/
 		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
+	}
+	
+	public static void samplePopulate(Connection conn) {
+		return;
 	}
 }
--- GameLibrary/src/org/elsys/GameLibrary/Main.java
+++ GameLibrary/src/org/elsys/GameLibrary/Main.java
@@ -7,14 +7,7 @@ public class Main {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String connectionString = "jdbc:mysql://localhost:3306/?user=root&password=root";
-			Connection conn = DriverManager.getConnection(connectionString);
-			
-			Statement createQuery = conn.createStatement();
-			ResultSet rs = createQuery.executeQuery("SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'GameLibrary'");
-			rs.next();
-			if(rs.getInt("COUNT(*)") <= 0) {
-				GameLibrary.createDatabase(conn);
-			}
+			GameLibrary.createDatabase(DriverManager.getConnection(connectionString));
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
