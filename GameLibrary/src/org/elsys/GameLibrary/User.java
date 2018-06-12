ce168228b46b3593ec43c94e7ca63a132bd4cc5d
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-06-12 20:26:08 +0300

    Added User.add, User.select, User table create

---
+++ GameLibrary/src/org/elsys/GameLibrary/User.java
@@ -0,0 +1,117 @@
+package org.elsys.GameLibrary;
+import java.sql.*;
+
+public class User {
+	private String userName;
+	private String realName;
+	private String passHash;
+	private int age;
+	private int id;
+	
+	public User(int id, String userName, String realName, String passHash, int age) {
+		this.id = id;
+		this.userName = userName;
+		this.realName = realName;
+		this.passHash = passHash;
+		this.age = age;
+	}
+	
+	public User(String userName, String realName, String passHash, int age) {
+		this.userName = userName;
+		this.realName = realName;
+		this.passHash = passHash;
+		this.age = age;
+	}
+	
+	public String getRealName() {
+		return this.realName;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getPassHash() {
+		return this.passHash;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public static void createUserTable(Connection conn) throws SQLException {
+		Statement query = null;
+		try {
+			query = conn.createStatement();
+			query.execute("CREATE TABLE Users ("
+					+ "Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+					+ "RealName VARCHAR(100) NOT NULL,"
+					+ "UserName VARCHAR(100) NOT NULL,"
+					+ "Age INTEGER NOT NULL,"
+					+ "PasswordHash VARCHAR(250) NOT NULL,"
+					+ "CONSTRAINT U_User UNIQUE (UserName, "
+					+ "PasswordHash)"
+					+ ");");
+			conn.setAutoCommit(false);
+		}catch (Exception e) {
+			System.out.print(e.getMessage());
+		}finally {
+			if(query != null) {
+				query.close();
+			}
+		}
+	}
+	
+	public static void addUser(Connection conn, User user) throws SQLException {
+		PreparedStatement prp = null;
+		conn.setAutoCommit(false);
+		try {
+			String insertSql = "INSERT INTO Users "
+					+ "(RealName, UserName, Age, PasswordHash)"
+					+ "VALUES(?, ?, ?, ?)";
+			prp = conn.prepareStatement(insertSql);
+			prp.setString(1, user.getRealName());
+			prp.setString(2, user.getUserName());
+			prp.setInt(3, user.getAge());
+			prp.setString(4, user.getPassHash());
+			prp.executeUpdate();
+			conn.commit();
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+			conn.rollback();
+		} finally {
+			conn.setAutoCommit(false);
+			if (prp != null) {
+				prp.close();
+			}
+		}
+	}
+	
+	public static User getUser(Connection conn, String userName, String passHash) throws SQLException {
+		PreparedStatement prp = null;
+		conn.setAutoCommit(false);
+		try {
+			String selectString = "SELECT * FROM Users WHERE UserName = ? AND PasswordHash = ?";
+			prp = conn.prepareStatement(selectString);
+			prp.setString(1, userName);
+			prp.setString(2, passHash);
+			ResultSet rs = prp.executeQuery();
+			rs.next();
+			String realname = rs.getString("RealName");
+			int age = rs.getInt("Age");
+			int id = rs.getInt("Id");
+			return new User(id, userName, realname, passHash, age);
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+			return null;
+		} finally {
+			if (prp != null) {
+				prp.close();
+			}
+		}
+	}
+}
