ed5ebab0497459c7106e459ad979666ce8c47993
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-06-14 22:03:13 +0300

    Added menu

--- GameLibrary/src/org/elsys/GameLibrary/Main.java
+++ GameLibrary/src/org/elsys/GameLibrary/Main.java
@@ -7,35 +7,47 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		/*try {
+		Connection conn = null;
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String connectionString = "jdbc:mysql://localhost:3306/?user=root&password=root";
 			//GameLibrary.createDatabase(DriverManager.getConnection(connectionString));
 			connectionString = "jdbc:mysql://localhost:3306/GameLibrary?user=root&password=root";
-			Connection conn = DriverManager.getConnection(connectionString);
+			conn = DriverManager.getConnection(connectionString);
 			//GameLibrary.samplePopulate(conn);
 			//GameLibrary.showUserGames(new User(1, "Toniuyt", "Antonio Milev", "kekeckekec", 17), conn);
-			GameLibrary.showAllGames(conn);
+			//GameLibrary.showAllGames(conn);
+		
+			BackMenu back = new BackMenu("0", "Back", null);
+			ExitMenu exit = new ExitMenu("0", "Exit", null);
+			RegisterMenu register = new RegisterMenu("1", "Register", null);
+			MainMenu main = new MainMenu("-", "Main Menu", new ArrayList<Menu>(Arrays.asList(exit, register)));
+	//		/main.show();
+			Menu oldMenu = main, currMenu =  main;
+			Scanner in = new Scanner(System.in);
+			boolean shown = false;
+			while(true) {
+				if(!shown) {
+					System.out.print("\n\n\n\n\n");
+					currMenu.show();
+					shown = true;
+				}
+				if(in.hasNext()) {
+					Object res = currMenu.action(oldMenu, in);
+					if(res instanceof User)
+					{
+						User.addUser(conn, (User) res);
+						currMenu = oldMenu;
+					}else if(res instanceof Menu) {
+						currMenu = (Menu) res;
+					}else if(res instanceof ExitMenu) {
+						break;
+					}
+					shown = false;
+				}
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-		}*/
-		BackMenu back = new BackMenu("0", "Back", null);
-		RegisterMenu register = new RegisterMenu("1", "Register", new ArrayList<Menu>(Arrays.asList(back)));
-		MainMenu main = new MainMenu("-", "Main Menu", new ArrayList<Menu>(Arrays.asList(back, register)));
-//		/main.show();
-		Menu oldMenu = main, currMenu =  main;
-		Scanner in = new Scanner(System.in);
-		boolean shown = false;
-		while(true) {
-			if(!shown) {
-				System.out.print("\n\n\n\n\n");
-				currMenu.show();
-				shown = true;
-			}
-			if(in.hasNext()) {
-				currMenu = currMenu.action(oldMenu, in.next());
-				shown = false;
-			}
 		}
 	}
 	
